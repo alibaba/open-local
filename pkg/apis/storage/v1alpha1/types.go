cf53b35718bd63eb041a93d5ea536bad65d4cfd5
TheBeatles1994 <huizhi.szh@alibaba-inc.com>
2021-07-28 17:09:59 +0800

    add missing copyright headers in some files

--- pkg/apis/storage/v1alpha1/types.go
+++ pkg/apis/storage/v1alpha1/types.go
@@ -1,5 +1,5 @@
 /*
-Copyright The Alipay inc Authors.
+Copyright 2021 OECP Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

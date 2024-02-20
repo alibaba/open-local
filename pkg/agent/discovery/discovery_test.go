6bc30467881d192bdb3fed8a21471dcb7b4ab471
Yuan Peng <ypnuaa037@126.com>
2024-02-20 15:27:48 +0800

    Reduce pressure to apiserver in large cluster
    The checkSPDKSupport frequency is too high, which puts a lot of pressure on apiserver in large cluster

--- pkg/agent/discovery/discovery_test.go
+++ pkg/agent/discovery/discovery_test.go
@@ -17,7 +17,10 @@ limitations under the License.
 package discovery
 
 import (
+	localv1alpha1 "github.com/alibaba/open-local/pkg/apis/storage/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
+	"time"
 )
 
 func TestFilterInfo(t *testing.T) {
@@ -80,4 +83,117 @@ func sameStringSlice(x, y []string) bool {
 		}
 	}
 	return len(diff) == 0
+}
+
+func TestIsNlsStatusChanged(t *testing.T) {
+	type args struct {
+		oldStatus *localv1alpha1.NodeLocalStorageStatus
+		newStatus *localv1alpha1.NodeLocalStorageStatus
+	}
+	updatedTime := metav1.Now()
+	oldStatus := &localv1alpha1.NodeLocalStorageStatus{
+		NodeStorageInfo: localv1alpha1.NodeStorageInfo{
+			DeviceInfos: []localv1alpha1.DeviceInfo{
+				{
+					Name: "/dev/sdb",
+					MediaType: "hdd",
+					Total: 100000000,
+				},
+			},
+			VolumeGroups: []localv1alpha1.VolumeGroup{
+				{
+					Name: "pool0",
+					PhysicalVolumes: []string{
+						"/dev/sdb",
+					},
+					Total: 100000000,
+					Allocatable: 100000000,
+					Available: 100000000,
+				},
+			},
+			Phase: "Running",
+			State: localv1alpha1.StorageState{
+				Status: localv1alpha1.ConditionTrue,
+				Type: localv1alpha1.StorageReady,
+			},
+		},
+		FilteredStorageInfo: localv1alpha1.FilteredStorageInfo{
+			UpdateStatus: localv1alpha1.UpdateStatusInfo{
+				LastUpdateTime: &updatedTime,
+				Status: "accepted",
+			},
+			VolumeGroups: []string{"pool0"},
+		},
+	}
+
+	newStatus := &localv1alpha1.NodeLocalStorageStatus{
+		NodeStorageInfo: localv1alpha1.NodeStorageInfo{
+			DeviceInfos: []localv1alpha1.DeviceInfo{
+				{
+					Name: "/dev/sdb",
+					MediaType: "hdd",
+					Total: 100000000,
+				},
+			},
+			VolumeGroups: []localv1alpha1.VolumeGroup{
+				{
+					Name: "pool0",
+					PhysicalVolumes: []string{
+						"/dev/sdb",
+					},
+					Total: 100000000,
+					Allocatable: 50000000,  // changed
+					Available: 50000000,    // changed
+				},
+			},
+			Phase: "Running",
+			State: localv1alpha1.StorageState{
+				Status: localv1alpha1.ConditionTrue,
+				Type: localv1alpha1.StorageReady,
+			},
+		},
+		FilteredStorageInfo: localv1alpha1.FilteredStorageInfo{
+			UpdateStatus: localv1alpha1.UpdateStatusInfo{
+				LastUpdateTime: &updatedTime,
+				Status: "accepted",
+			},
+			VolumeGroups: []string{"pool0"},
+		},
+	}
+
+	newStatusOnlyTimeChanged := oldStatus
+	newUpdateTime := updatedTime.Add(1*time.Minute)
+	newStatusOnlyTimeChanged.FilteredStorageInfo.UpdateStatus.LastUpdateTime = &metav1.Time{Time: newUpdateTime}
+	newStatusOnlyTimeChanged.NodeStorageInfo.State.LastTransitionTime = &metav1.Time{Time: newUpdateTime}
+	newStatusOnlyTimeChanged.NodeStorageInfo.State.LastHeartbeatTime = &metav1.Time{Time: newUpdateTime}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test1",
+			args: args{
+				oldStatus: oldStatus,
+				newStatus: newStatus,
+			},
+			want: true,
+		},
+		{
+			name: "test2",
+			args: args{
+				oldStatus: oldStatus,
+				newStatus: newStatusOnlyTimeChanged,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isNlsStatusChanged(tt.args.oldStatus, tt.args.newStatus); got != tt.want {
+				t.Errorf("isNlsStatusChanged() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
